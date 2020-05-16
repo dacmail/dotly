d75f098947763d5df8bb0361ffc4918dbd779418
Daniel Aguilar <dacmail@gmail.com>
2020-05-16 16:31:10 +0200

    moving from deprecated ruby installer to bash

--- scripts/self/utils/install.sh
+++ scripts/self/utils/install.sh
@@ -3,7 +3,7 @@
 install_macos_custom() {
   # Install brew if not installed
   if ! [ -x "$(command -v brew)" ]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   fi
 
   # All apps (This line is 2 times because there are dependencies between brew cask and brew)
